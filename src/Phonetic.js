d7090be90d82f2b9b246884a60240d8094cb77b2
Lavirene <108747106+Lavirene@users.noreply.github.com>
2022-10-14 12:36:46 +0300

    Phonetics target blank fix

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
   if (props.phonetic.audio) {
     return (
       <div className="Phonetics">
-        <a href={props.phonetic.audio} targret="_blank">
+        <a href={props.phonetic.audio} target="_blank">
           Listen
         </a>
         <br />
