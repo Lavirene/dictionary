518f7e555d540ae7bce237a33b987a201addb3d2
Lavirene <108747106+Lavirene@users.noreply.github.com>
2022-10-14 14:11:33 +0300

    Styling improvement

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,35 +1,55 @@
 import React, { useState } from "react";
+import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.keyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeyword(event) {
     setKeyword(event.target.value);
   }
-
-  return (
-    <div>
-      <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Type here... "
-          onChange={handleKeyword}
-        />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <h1>What do you want to look up?</h1>
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Type here... "
+              onChange={handleKeyword}
+              defaultValue={keyword}
+            />
+          </form>
+          <div className="hint">seggested keywords: life, love, infinity</div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
