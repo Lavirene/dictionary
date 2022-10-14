ab7468c97ac55af613c02ec5b4e5e46b91d6e5bb
Lavirene <108747106+Lavirene@users.noreply.github.com>
2022-10-14 16:08:41 +0300

    Pexels photos integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.keyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function load() {
     setLoaded(true);
@@ -17,9 +19,18 @@ export default function Dictionary(props) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = `563492ad6f91700001000001608ba76674d04886979d0c3d50940e4b`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -46,6 +57,7 @@ export default function Dictionary(props) {
           <div className="hint">seggested keywords: life, love, infinity</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} keyword={keyword} />
       </div>
     );
   } else {
